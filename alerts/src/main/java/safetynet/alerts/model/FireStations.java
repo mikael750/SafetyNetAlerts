7fa27393ffa70822a730c0c4534060a63fc2fedf
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-02-16 11:58:02 +0100

    MHE - Complete models

--- alerts/src/main/java/safetynet/alerts/model/FireStations.java
+++ alerts/src/main/java/safetynet/alerts/model/FireStations.java
@@ -1,4 +1,48 @@
 package safetynet.alerts.model;
 
 public class FireStations {
+    private int id;
+    private String address;
+    private String station;
+
+    public FireStations(){}
+
+    public FireStations(int id, String address, String station){
+        this.id = id;
+        this.address = address;
+        this.station = station;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getStation(){
+        return station;
+    }
+
+    public void setStation(String station) {
+        this.station = station;
+    }
+
+    @Override
+    public String toString() {
+        return "firestations{" +
+                "id=" + id +
+                ", address='" + address + '\'' +
+                ", station=" + station +
+                '}';
+    }
 }
--- alerts/src/main/java/safetynet/alerts/model/MedicalRecords.java
+++ alerts/src/main/java/safetynet/alerts/model/MedicalRecords.java
@@ -1,4 +1,81 @@
 package safetynet.alerts.model;
 
 public class MedicalRecords {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String birthdate;
+    private String[] medications;
+    private String[] allergies;
+
+    public MedicalRecords(){}
+
+    public MedicalRecords(int id, String firstName, String lastName, String birthdate, String[] medications, String[] allergies){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdate = birthdate;
+        this.medications = medications;
+        this.allergies = allergies;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String[] getMedications() {
+        return medications;
+    }
+
+    public void setMedications(String[] medications) {
+        this.medications = medications;
+    }
+
+    public String[] getAllergies() {
+        return allergies;
+    }
+
+    public void setAllergies(String[] allergies) {
+        this.allergies = allergies;
+    }
+
+    @Override
+    public String toString() {
+        return "medicalrecords{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthdate='" + birthdate + '\'' +
+                ", medications='" + medications + '\'' +
+                ", allergies=" + allergies +
+                '}';
+    }
 }
